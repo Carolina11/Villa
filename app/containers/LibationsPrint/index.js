555c82499e949f3eca822d5436ee065060d876cd
Ed <Ed@Eds-MacBook-Air.local>
2017-11-08 10:20:13 -0500

    allSpecials

--- app/containers/LibationsPrint/index.js
+++ app/containers/LibationsPrint/index.js
@@ -12,32 +12,64 @@ import './styleM.css';
 import * as GLOBAL from '../../globals';
 
 export default class LibationsPrint extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      allSpecials: []
+    }
+  }
+  componentWillMount() {
+    this.getAllSpecials();
+  }
+
+  getAllSpecials = () => {
+    fetch(GLOBAL.BASE_URL + '/api/searchSpecials', {
+      method: 'post'
+    })
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      this.setState({
+        allSpecials:json.searchSpecials
+      })
+    }.bind(this));
+  };
+
+
+
   render() {
+    this.state.allSpecials.map((special,index)=>(
+        `type => ${special.typeID}`,
+        `name => ${special.name}`,
+        `price => ${special.price}`,
+        `description => ${special.description}`,
+        `quantity => ${special.quantity}`,
+        `pairings => ${special.pairings}`,
+
+        special.ingredient
+        ]);
+  ))
     return (
       <div className="container">
         <Helmet title="LibationsPrint" meta={[ { name: 'description', content: 'Description of LibationsPrint' }]}/>
 
-        <div className="outLine">
-          <h1>Libations</h1>
-          <div className="specials">
+          {this.state.allSpecials.map((special,index)=>(
+            <div>
+              <div>DB::table('specials')->insert([</div>
+              <div>'type' => '{special.typeID}',</div>
+              <div>'name' => '{special.name}',</div>
+              <div>'price' => '{special.price}',</div>
+              <div>'description' => '{special.description}',</div>
+              <div>'quantity' => '{special.quantity}',</div>
+              <div>'pairings' => '{special.pairings}',</div>
 
-            <div className="entrees">
-              <h2>*** Entrees ***</h2>
-            </div>
-            <div className="desserts">
-              <h2>*** Desserts ***</h2>
-            </div>
-            <div className="libations">
-              <h2>Beer News<br />
-              Samuel Adams Seasonal is ***</h2>
-              <h2>Traveling Beer Tap is ***</h2>
-            </div>
-            <div className="notes">
-              <h2></h2>
-            </div>
-          </div>
-        </div>
+              <div>'ingredient' => '{special.ingredient ? special.ingredient : null }',</div>
 
+              <div>]);</div>
+              <div><br/></div>
+            </div>
+        ))}
 
 
 

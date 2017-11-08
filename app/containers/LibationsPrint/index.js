2bd48c3164f62767463de0a31f4583896e299745
Ed <Ed@Eds-MacBook-Air.local>
2017-11-08 13:17:37 -0500

    going back

--- app/containers/LibationsPrint/index.js
+++ app/containers/LibationsPrint/index.js
@@ -30,6 +30,7 @@ export default class LibationsPrint extends React.PureComponent {
       return response.json();
     })
     .then(function(json){
+      console.log(json.searchSpecials);
       this.setState({
         allSpecials:json.searchSpecials
       })
@@ -39,46 +40,105 @@ export default class LibationsPrint extends React.PureComponent {
 
 
   render() {
-    this.state.allSpecials.map((special,index)=>(
-        `type => ${special.typeID}`,
-        `name => ${special.name}`,
-        `price => ${special.price}`,
-        `description => ${special.description}`,
-        `quantity => ${special.quantity}`,
-        `pairings => ${special.pairings}`,
+    const allSpecials = this.state.allSpecials.map((special) => {
+      /*const type = `type => ${special.typeID}`;
+      const name = `name => ${special.name}`;
+      const price = `price => ${special.price}`;
+      const description = `description => ${special.description}`;
+      const quantity = `quantity => ${special.quantity}`;
+      const pairings = `pairings => ${special.pairings}`;
+      const ingredient = special.ingredient ? `ingredient => ${special.ingredient}` : ''*/
+      const ingredient = special.ingredient ? special.ingredient : ''
+      if (ingredient) {
+        return [
+          `type => ${special.typeID}`,
+          `name => ${special.name}`,
+          `price => ${special.price}`,
+          `description => ${special.description}`,
+          `quantity => ${special.quantity}`,
+          `pairings => ${special.pairings}`,
+          `ingredient => ${special.ingredient}`
+        ];
+      } else {
+        return [
+          `type => ${special.typeID}`,
+          `name => ${special.name}`,
+          `price => ${special.price}`,
+          `description => ${special.description}`,
+          `quantity => ${special.quantity}`,
+          `pairings => ${special.pairings}`,
+        ]
+      }
 
-        special.ingredient
-        ]);
-  ))
+    });
     return (
       <div className="container">
+      {console.log(allSpecials)}
         <Helmet title="LibationsPrint" meta={[ { name: 'description', content: 'Description of LibationsPrint' }]}/>
-
-          {this.state.allSpecials.map((special,index)=>(
+          {allSpecials.map((allSpecial,index) => (
+            <div>
+              {console.log(`type:${allSpecial.type}`)}
+              <div>DB::table('specials')->insert([</div>
+              <div>{allSpecial.type},</div>
+              <div>{allSpecial.name},</div>
+              <div>{allSpecial.price},</div>
+              <div>{allSpecial.description},</div>
+              <div>{allSpecial.quantity},</div>
+              <div>{allSpecial.pairings},</div>
+              <div>{allSpecial.ingredient}</div>}
+              <div>]);</div>
+              <br />
+            </div>
+          ))}
+          {allSpecials.map((allSpecial, index) => (
             <div>
               <div>DB::table('specials')->insert([</div>
-              <div>'type' => '{special.typeID}',</div>
-              <div>'name' => '{special.name}',</div>
-              <div>'price' => '{special.price}',</div>
-              <div>'description' => '{special.description}',</div>
-              <div>'quantity' => '{special.quantity}',</div>
-              <div>'pairings' => '{special.pairings}',</div>
-
-              <div>'ingredient' => '{special.ingredient ? special.ingredient : null }',</div>
-
+              <div>{allSpecial.type},</div>
+              <div>{allSpecial.name},</div>
+              <div>{allSpecial.price},</div>
+              <div>{allSpecial.description},</div>
+              <div>{allSpecial.quantity},</div>
+              <div>{allSpecial.pairings},</div>
+              <div>{allSpecial.ingredient}</div>}
               <div>]);</div>
-              <div><br/></div>
+              <br />
             </div>
-        ))}
-
-
-
-
+          ))}
       </div>
     );
   }
 }
 
+
+/*const addOne = x => x + 1;
+const addTwo = (x,y) => {
+  x = x + 1;
+  y = y + 1;
+  return x + y;
+}*/
+
+
+
+
+
 LibationsPrint.contextTypes = {
   router: React.PropTypes.object
 };
+
+
+
+
+/*{this.state.allSpecials.map((special,index)=>(
+  <div>
+    <div>DB::table('specials')->insert([</div>
+    <div>'type' => '{special.typeID}',</div>
+    <div>'name' => '{special.name}',</div>
+    <div>'price' => '{special.price}',</div>
+    <div>'description' => '{special.description}',</div>
+    <div>'quantity' => '{special.quantity}',</div>
+    <div>'pairings' => '{special.pairings}',</div>
+
+    <div>'ingredient' => '{special.ingredient ? special.ingredient : null }',</div>
+
+  <div>]);</div>
+  <div><br/></div>*/
